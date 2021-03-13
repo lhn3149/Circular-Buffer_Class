bc564daebf74e95faea71e1bdb61c2ca24484327
Ryan Bowen <rmbeee@rit.edu>
2021-03-13 02:02:26 +0000

    Initial commit

---
+++ CharCircularBuffer.cpp
@@ -0,0 +1,20 @@
+#include <memory>
+#include <iostream>
+#include "CharCircularBuffer.h"
+using namespace std;
+
+CharCircularBuffer::CharCircularBuffer(int capacity)
+    : capacity(capacity), buffer(unique_ptr<char[]>(new char[capacity]))
+{
+    reset();
+}
+
+void CharCircularBuffer::printStatus() const
+{
+    cout << "------------------------------------\n";
+    cout << "Buffer Capacity: " <<  getCapacity() << endl;
+    cout << "Buffer Size: " <<  getSize() << endl;
+    cout << "Buffer isEmpty: " << isEmpty() << endl;
+    cout << "Buffer isFull: " << isFull() << endl;
+    cout << "------------------------------------\n";
+}
